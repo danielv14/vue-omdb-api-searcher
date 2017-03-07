93ae81bffca3e6983da04488424c3c56bca2f43f
Daniel <daniel.vernberg@gmail.com>
2017-03-07 08:58:49 +0100

    fully using a store for each of the components to interact with

--- src/main.js
+++ src/main.js
@@ -3,11 +3,14 @@
 import Vue from 'vue'
 import App from './App'
 
+import store from './store'
+
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
   template: '<App/>',
-  components: { App }
+  components: { App },
+  store // make store available to all components etc
 })
